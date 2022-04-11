701b83090f09f0dd227687be797d2f6de3e6ff3d
Domagoj Licitar <licitar.domagoj@gmail.com>
2022-04-11 17:32:39 +0200

    Add project files.

---
+++ ImenikOsoba/Controllers/OsobeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ImenikOsoba;
+
+namespace ImenikOsoba.Controllers
+{
+    public class OsobeController : Controller
+    {
+        private ImenikDbContext _dbContext = new ImenikDbContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            _dbContext.Dispose();    
+        }
+
+        public ActionResult Index()
+        {
+            return View(_dbContext.Osobes.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var osobe = _dbContext.Osobes.SingleOrDefault(i => i.Id == id);
+            
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (osobe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(osobe);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Osobe osobe)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Osobes.Add(osobe);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(osobe);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var osobe = _dbContext.Osobes.SingleOrDefault(i => i.Id == id);
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            if (osobe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(osobe);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Osobe osobe)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(osobe).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(osobe);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var osobe = _dbContext.Osobes.SingleOrDefault(i => i.Id == id);
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (osobe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(osobe);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var osobe = _dbContext.Osobes.SingleOrDefault(i => i.Id == id);
+            _dbContext.Osobes.Remove(osobe);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ImenikOsoba/ViewModels/OsobeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImenikOsoba.ViewModels
+{
+    public class OsobeViewModel
+    {
+        public Osobe Osobe { get; set; }
+    }
+}
